89b0f5b3551666a054946d8c1168dfe702ae0be0
Martin Werner <m.werner.0@protonmail.com>
2023-12-24 16:50:23 +0100

    Update git_new

--- observer.py
+++ observer.py
@@ -27,6 +27,15 @@ class TrackedFile:
         self.path = path
         self.modDate = getmtime(self.path)
 
+    def __eq__(self, other):
+        return self.__hash__() == other.__hash__()
+
+    def __ne__(self, other):
+        return not __eq__(self, other)
+
+    def __hash__(self):
+        return hash(self.path)
+
     # observe if the world has been opened
     def update(self):
         currentModDate = getmtime(self.path)
@@ -52,20 +61,40 @@ class TrackedFile:
         self.modDate = currentModDate
 
 
-
-def main():
-    # pull latest state
-    gitPull()
-
-    # find files to track
+def findTrackableFiles():
     filePaths = [f for f in listdir() if isfile(f)]
-
     for f in listdir():
         if isdir(f):
             filePaths += [join(f, g) for g in listdir(f) if isfile(join(f, g))]
-
     trackedFiles = [TrackedFile(f) for f in filePaths
                     if f.find("git_") != -1 and f.find(".db") != -1]
+    return set(trackedFiles)
+
+
+def loop():
+    trackedFiles = set()
+
+    while True:
+        if activeWorld == 0:
+            trackedFiles = trackedFiles.union(findTrackableFiles())
+            
+            print("worlds found:")
+            for f in trackedFiles:
+                print(f.path)
+            print()
+            
+            for f in trackedFiles:
+                f.update()
+        else:
+            activeWorld.checkClosed()
+        time.sleep(2.0)
+
+
+def main():
+
+    gitPull()
+
+    trackedFiles = findTrackableFiles()
 
     print("worlds found:")
     for f in trackedFiles:
@@ -73,17 +102,9 @@ def main():
     print()
 
     # launch Scrap Mechanic
-    system("steam steam://rungameid/387990")
+    # system("steam steam://rungameid/387990")
 
-    # observation loop
-    while True:
-        if activeWorld == 0:
-            for f in trackedFiles:
-                f.update()
-        else:
-            activeWorld.checkClosed()
-        time.sleep(2.0)
-
+    loop()
 
 
 if __name__ == "__main__":
